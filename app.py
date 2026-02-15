0cbb4edcf6fed3cd11e601c6b70c7e9f5f8deaac
HiroakiNakano1985 <hiroakinakano.hj@gmail.com>
2026-02-15 15:17:32 +0100

    made minor changes

--- app.py
+++ app.py
@@ -4,8 +4,24 @@ import pandas as pd
 from datetime import time
 from config import origins, origin_info, airline_names
 
-st.title("Barcelona Flight Optimizer")
+def format_duration(iso_duration):
+    hours = 0
+    minutes = 0
 
+    if "H" in iso_duration:
+        hours = int(iso_duration.split("T")[1].split("H")[0])
+    if "M" in iso_duration:
+        minutes = int(iso_duration.split("H")[-1].replace("M", ""))
+
+    return f"{hours}:{minutes:02d}"
+
+def highlight_cheapest(row):
+    if row["is_cheapest"]:
+        return ["background-color: #d1ffd1"] * len(row)
+    return [""] * len(row)
+
+
+st.title("Barcelona Flight Optimizer")
 st.subheader("UC1: Search flights arriving in Barcelona")
 
 # date
@@ -71,6 +87,13 @@ if st.button("Search"):
 if "df" in st.session_state:
     df = st.session_state["df"].copy()
 
+    df["City"] = df["origin"].apply(lambda x: origin_info[x][0])
+    df["Country"] = df["origin"].apply(lambda x: origin_info[x][1])
+    df["AirlineName"] = df["airline"].apply(lambda x: airline_names.get(x, "Unknown"))
+    df["Duration"] = df["duration"].apply(format_duration)
+
+    df = df.drop(columns=["departure", "arrival"])
+
     
     df["dep_time"] = pd.to_datetime(df["departure"]).dt.time
     df["arr_time"] = pd.to_datetime(df["arrival"]).dt.time
@@ -81,6 +104,11 @@ if "df" in st.session_state:
         (df["arr_time"] >= arr_start) &
         (df["arr_time"] <= arr_end)
     ]
+
+    filtered["is_cheapest"] = filtered.groupby("origin")["price"].transform(
+    lambda x: x == x.min())
+    st.dataframe(filtered.style.apply(highlight_cheapest, axis=1))
+
 
     st.subheader("Filtered Results")
     st.dataframe(filtered)
