70e6fbd3f11651a3ed257e05911a43a644a067d5
HiroakiNakano1985 <hiroakinakano.hj@gmail.com>
2026-02-17 19:47:16 +0100

    drawing dataset by using API

--- run_test.py
+++ run_test.py
@@ -1,10 +1,13 @@
 from data_generation import collect_all_cities
 
 # 20 cities
-origins = [
-    "LIS", "CMN", "TUN", "CDG", "LON", "ZRH", "BRU", "AMS",
-    "BER", "PRG", "WAW", "VIE", "LJU", "FCO", "BUD", "ZAG",
-    "OTP", "SOF", "ATH", "IST", "NCE", "MAD"]
+#origins = [
+#    "LIS", "CMN", "TUN", "CDG", "LON", "ZRH", "BRU", "AMS",
+#    "BER", "PRG", "WAW", "VIE", "LJU", "FCO", "BUD", "ZAG",
+#    "OTP", "SOF", "ATH", "IST", "NCE", "MAD"]
+
+origins = ["NCE", "MAD"]
+
 
 # run for 30days
 collect_all_cities(origins, "BCN", "2026-02-18", days=30)
